7f4d5a576391bd7cb8a6a6fc6ca6e2e5b5bc533b
makaroszkaa <76106848+makaroszkaa@users.noreply.github.com>
2022-05-24 08:58:27 +0200

    add: raw products computation

--- ga_ckd_transform.R
+++ ga_ckd_transform.R
@@ -2,9 +2,12 @@ pkgs <- c("ggplot2", "data.table")
 dpnd <- lapply(pkgs, library, character.only = T)
 rm(pkgs, dpnd)
 
+# Define custom functions
+my_summary <- function(i){return(round(mean(i), 2))}
+
+# Read raw data
 foods    <- yaml::read_yaml('food_dictionary.yaml')
 raw_data <- fread("food.csv", stringsAsFactors = F)
-food_qty <- fread("food_quantities.csv", stringsAsFactors = F)
 raw_data <- raw_data[, c(1, 2, 12, 18, 23, 29, 30, 31)]
 raw_data <- na.omit(raw_data)
 col_name <- c("cat", "prod", "enrg", "pro", "carb", "fat", "mass", "units")
@@ -31,25 +34,73 @@ rm(cols)
 fltr_data <- raw_data[cat %in% cats, ]
 fltr_data <- dplyr::arrange(fltr_data, cat, desc(enrg), pro)
 
+
+### ---               Compute mean nutrition facts for raw foods         --- ###
+
 # Extract raw foods to exclude semi-products programmatically where possible
 raw_foods <- fltr_data[grep("raw", fltr_data$prod), ]
 
 # Check within group variation of each category to decide how to trim mean
 # values of each.
-tmp <- copy(raw_foods)
-tmp[, as.list(summary(enrg)), by = "cat"]
+tmp     <- copy(raw_foods)
+tmp_smr <- tmp[, as.list(summary(enrg)), by = "cat"]
 
+# Find out which tail is longer to allow the variation optimization
+tmp_skewness <- tmp[, as.list(moments::skewness(enrg)), by = "cat"]
+colnames(tmp_skewness)[2] <- "skew"
+tmp_skewness$skew[is.nan(tmp_skewness$skew)] <- 0
+tmp_skewness$skew <- round(tmp_skewness$skew, 2)
+
+# Compute variation coefficients to identify categories with wrong data
 tmp_cv <- tmp[, .(sd_e = sd(enrg), mn_e = mean(enrg)), by = cat]
+tmp_cv <- merge(tmp_cv, tmp_skewness, by = "cat", all.x = T)
 tmp_cv[is.na(tmp_cv)] <- 0
 tmp_cv[, cv_e := round(sd_e / mn_e, 2)]
-tmp_cv$cat[tmp_cv$cv_e > 0.30]
+cv_cats <- tmp_cv$cat[tmp_cv$cv_e > 0.30]
+names(cv_cats) <- cv_cats
 
-# TODO: trim categories that have high variation
+# Trim categories which have variation more than 30 percent
+trim_res <- lapply(
+  X = cv_cats,
+  FUN = function(i){
+    temp_cv <- tmp_cv[cat == i, cv_e]
+    while (temp_cv > 0.20){
+      temp_df <- tmp[cat == i, ]
+      temp_qt <- quantile(temp_df$enrg,
+                          ifelse(tmp_cv[cat == i, skew] > 0, 0.9, 0.1))
+      text_fl <- ifelse(test = tmp_cv[cat == i, skew] > 0, 
+                        yes  = "enrg <= temp_qt",
+                        no   = "enrg >= temp_qt")
+      temp_df <- temp_df[eval(parse(text = text_fl)), ]
+      temp_cv <- temp_df[, .(sd_e = sd(enrg), mn_e = mean(enrg)), by = cat]
+      temp_cv[is.na(temp_cv)] <- 0
+      temp_cv[, cv_e := round(sd_e / mn_e, 2)]
+      temp_cv <- temp_cv[, cv_e]
+      print(paste('variation of', i, temp_cv))
+      text_fl <- ifelse(test = tmp_cv[cat == i, skew] > 0,
+                        yes  = "!(cat == i & enrg >= temp_qt)",
+                        no   = "!(cat == i & enrg <= temp_qt)")
+      tmp     <- tmp[eval(parse(text = text_fl)), ]
+    }
+    return(temp_cv)
+  }
+)
+
+# Group raw foods by category and compute mean
+raw_foods <- copy(tmp)
+raw_foods[, units := NULL]
+raw_foods[, prod := NULL]
+raw_foods <- raw_foods[, lapply(.SD, my_summary), by = cat]
+
+rm(tmp, tmp_cv, tmp_smr, trim_res, tmp_skewness, cv_cats)
+
 # TODO: find solution for categories which do not have raw attribute
 
 # Check which categories did not fall into raw foods
 idx     <- !(unique(fltr_data$cat) %in% unique(raw_foods$cat))
 not_raw <- unique(fltr_data$cat)[idx]
+
+
 
 
 
@@ -65,13 +116,7 @@ grp_data <- fltr_raw[
   by = category
 ]
 
-# Compute menu energy and protein
-menu <- merge(grp_data, food_qty, by = "category", all = T)
-menu[
-  , energy := round(unit_energy * qty / 100, 1)
-][
-  , protein := round(unit_protein * qty / 100, 1)
-]
+
 
 
 
