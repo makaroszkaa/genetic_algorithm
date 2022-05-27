c1a3d7e83d47ac70d6a3748a626623debffedbf0
makaroszkaa <76106848+makaroszkaa@users.noreply.github.com>
2022-05-27 09:03:39 +0200

    update: to do list for stages dependent limitations

--- ga_ckd_diet.R
+++ ga_ckd_diet.R
@@ -6,11 +6,14 @@ source("ga_helpers.R")
 menu <- readRDS('menu_file.rds')
 
 # Define limitations
-wt_lim  <- 0.7 * 90         ## low protein diet is 0.6 - 0.8 grams per 1 kg
+# TODO: wt_lim depends on CKD stages
+# TODO: stages 1 -- 1.0, 2 -- 0.8, 3 -- 0.7 , 4 -- 0.6, 5 -- 0.6
+# TODO: add phosphorus and potassium restrictions
+wt_lim  <- 0.6 * 90         ## low protein diet is 0.6 - 0.8 grams per 1 kg
 a_energ <- (25  * 90) * 1.1 ## adequate energy for mid-level activity + 10%
 
 # Subset foods from menu
-sub_menu <- sample_sum(menu, a_energ, 12, T)
+sub_menu <- sample_sum(menu, a_energ, 123, T)
 
 # Make dataset
 col_nms <- c("cat", "cals", "pros")
@@ -31,7 +34,7 @@ best_chromosome <- extract_best(ga_model)
 
 # Evaluate best solution
 dataset[best_chromosome == 1, ]
-cat(paste(best_chromosome %*% dataset$pnts, "/", sum(dataset$pnts)))
+cat(paste(best_chromosome %*% dataset$pnts, "/", sum(dataset$pnts), "\n"))
 
 cat(paste(paste0(" Total protein: ", sum(dataset[best_chromosome == 1, wt])),
           "\n",
